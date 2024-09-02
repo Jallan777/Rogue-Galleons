64a6b1e0978d0102cbf6f9d9d3a03875231e3faa
Ryan Dang <datkiechen123@gmail.com>
2024-09-02 21:33:15 +1200

    1

--- My project (1)/Assets/Scripts/MainMenu/VolumeControl.cs
+++ My project (1)/Assets/Scripts/MainMenu/VolumeControl.cs
@@ -8,17 +8,17 @@ public class VolumeControl : MonoBehaviour
 
     void Start()
     {
-        // Set the slider to a value corresponding to a percentage (e.g., 65%)
+        //default setting
         volumeSlider.value = 65.0f;
 
-        // Add a listener to handle volume change
+       //changes
         volumeSlider.onValueChanged.AddListener(SetVolume);
     }
 
     public void SetVolume(float sliderValue)
     {
-        // Set the volume directly based on the slider's value normalized between 0 and 1
-        float normalizedValue = sliderValue / 100.0f;  // Converting from percentage (0 to 100) to the range Unity uses (0 to 1)
+      
+        float normalizedValue = sliderValue / 100.0f;  //convert to precentage
         audioSource.volume = normalizedValue;
     }
 }
