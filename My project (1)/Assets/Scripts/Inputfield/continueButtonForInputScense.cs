24b086e2d5b1cf27c7b3c50f53063c9900fd2a11
Ryan Dang <datkiechen123@gmail.com>
2024-09-13 13:22:14 +1200

    11
    1

--- My project (1)/Assets/Scripts/Inputfield/continueButtonForInputScense.cs
+++ My project (1)/Assets/Scripts/Inputfield/continueButtonForInputScense.cs
@@ -1,10 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class continu : MonoBehaviour
+
+
+
+public class contun : MonoBehaviour
 {
-   //SceneManager.LoadSceneAsync(1);
-  
+
+    public void contin()
+    {
+        SceneManager.LoadSceneAsync("Scene 1"); // load the game level
+
+    }
+
+
+
+
+
+
 }
