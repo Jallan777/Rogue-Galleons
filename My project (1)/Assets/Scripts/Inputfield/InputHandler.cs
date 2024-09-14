9dc7c26f5a736ac1e541b85ee207b6c37150ec33
Jacob Allan <jacobstuartallan@gmail.com>
2024-09-14 22:23:09 +1200

    New Scenes!
    Made 2 new scenes with animations, and made volume control work across scenes. Can now get from play button through to scene

--- My project (1)/Assets/Scripts/Inputfield/InputHandler.cs
+++ My project (1)/Assets/Scripts/Inputfield/InputHandler.cs
@@ -13,15 +13,21 @@ public class InputHandler : MonoBehaviour
     {
         string input = inputField.text;
 
-        if (input.Length < 3)
+        if (input.Length < 3 )
         {
-            resultText.text = "Invalid input, Please type longer";
+            resultText.text = "Invalid Input, Please type longer name";
+            resultText.color = Color.red;
+        }
+        else if(input.Length > 15)
+        {
+            resultText.text = "Invalid Input, Please type shorter name";
             resultText.color = Color.red;
         }
         else
         {
-            resultText.text = "welcome "+ input;
+            resultText.text = "Welcome <b>"+ input + "</b>";
             resultText.color = Color.blue;
+
         }
 
     }
