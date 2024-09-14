1a0647fd4a48997fdafcf9d9fb2d2829705dc10e
Ryan Dang <datkiechen123@gmail.com>
2024-09-14 18:52:14 +1200

    bg scence
    123

--- My project (1)/Assets/Scripts/Inputfield/continueButtonForInputScense.cs
+++ My project (1)/Assets/Scripts/Inputfield/continueButtonForInputScense.cs
@@ -12,7 +12,25 @@ public class contun : MonoBehaviour
 
     public void contin()
     {
-        SceneManager.LoadSceneAsync("Ingame"); // load the game level
+        int randomNumber = Random.Range(1, 4);
+        Debug.Log("Random number generated: " + randomNumber);
+
+        if (randomNumber == 1)
+        { 
+        SceneManager.LoadSceneAsync("Ingame"); 
+        
+        }
+        if (randomNumber == 2)
+        {
+            SceneManager.LoadSceneAsync("Ingame 2"); 
+
+        }
+        if (randomNumber == 3)
+        {
+            SceneManager.LoadSceneAsync("Ingame 3"); 
+
+        }
+
 
     }
 
