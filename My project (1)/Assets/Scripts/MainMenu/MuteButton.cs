cb75b10b5b07498a631da703af96d358c2d233e9
Jacob Allan <jacobstuartallan@gmail.com>
2024-09-19 22:22:49 +1200

    Fixed volue sliders and mute buttons again
    once again fixed the ever breaking music stuff

--- My project (1)/Assets/Scripts/MainMenu/MuteButton.cs
+++ My project (1)/Assets/Scripts/MainMenu/MuteButton.cs
@@ -4,17 +4,44 @@ using UnityEngine.UI;
 public class MuteButton : MonoBehaviour
 {
     public Button muteButton;
+    public Button unmuteButton;
     public AudioSource audioSource;
     private bool isMuted = false;
 
     void Start()
     {
-        muteButton.onClick.AddListener(ToggleMute);
+        //muteButton.gameObject.SetActive(true);
+        //unmuteButton.gameObject.SetActive(false);
+
+
+        muteButton.onClick.AddListener(Mute);
+        unmuteButton.onClick.AddListener(Unmute);
     }
 
     void ToggleMute()
     {
         isMuted = !isMuted;
         audioSource.mute = isMuted;
+    }
+
+    void Mute()
+    {
+        muteButton.gameObject.SetActive(false);
+        unmuteButton.gameObject.SetActive(true);
+
+        isMuted = true;
+        audioSource.mute = isMuted;
+
+
+    }
+
+    void Unmute()
+    { 
+        muteButton.gameObject.SetActive(true);
+        unmuteButton.gameObject.SetActive(false);
+        isMuted = false;
+        audioSource.mute = isMuted;
+
+
     }
 }
