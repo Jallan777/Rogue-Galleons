9b72e36f4aa2a95a1cdf103b9bb7c333e42da970
Jacob Allan <jacobstuartallan@gmail.com>
2024-10-18 05:31:25 +1300

    Map (Mostly) Works
    Islands mostly don't overlay. Treasure island doesn't move if no suitable location available

--- My project (1)/Assets/Scripts/General/CreateMap.cs
+++ My project (1)/Assets/Scripts/General/CreateMap.cs
@@ -9,6 +9,8 @@ public class CreateMap : MonoBehaviour
 
     public GameObject[] centerIslands;
     public GameObject[] edgeIslands;
+
+    public GameObject[] otherIslands;
     public Vector2 boundingAreaSize;
     public Vector2 boundingAreaCenter;
     public Image treasureIslandImage;
@@ -16,10 +18,16 @@ public class CreateMap : MonoBehaviour
     public Color boxColour = Color.red;
     private List<Vector2> placedPositions = new List<Vector2>();
 
+
+    public RectTransform image1;
+    public RectTransform image2; 
+
+
+
     // Start is called before the first frame update
     void Start()
     {
-        if(tiImages.Length != 3)
+        if (tiImages.Length != 3)
         {
             Debug.LogError("Assign 3 images please!");
             return;
@@ -27,42 +35,86 @@ public class CreateMap : MonoBehaviour
 
         AssignTIImg();
 
-        foreach(GameObject island in centerIslands)
+        foreach (GameObject island in centerIslands)
         {
             PlaceRandomly(island);
         }
 
-        foreach(GameObject island in edgeIslands)
+        foreach (GameObject island in edgeIslands)
         {
             PlaceRandomX(island);
-        }         
+        }
+
+        AddIslandPositions();
+    }
+
+    void AddIslandPositions()
+    {
+        
+        
+        foreach(GameObject island in otherIslands)
+        {
+            RectTransform rt = island.GetComponent<RectTransform>();
+            if(rt != null)
+            {
+                placedPositions.Add(rt.anchoredPosition);
+            }
+            else
+            {
+                Debug.LogWarning("No RectTransform found for island: " + island.name);
+            }
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        // Get the anchored positions
+        Vector2 pos1 = image1.anchoredPosition;
+        Vector2 pos2 = image2.anchoredPosition;
+
+        // Calculate distance
+        float distance = Vector2.Distance(pos1, pos2);
+
+        // Print distance to console
+        Debug.Log("Distance between images: " + distance);
     }
 
     void PlaceRandomly(GameObject island)
     {
         RectTransform rectTransform = island.GetComponent<RectTransform>();
 
-        if(rectTransform == null)
+        if (rectTransform == null)
         {
             Debug.LogError("No RectTransform found on island: " + island.name);
             return;
         }
 
-        Vector2 islandSize = new Vector2(rectTransform.rect.width, rectTransform.rect.height);
+        Vector2 islandSize = new Vector2(rectTransform.rect.width + 15, rectTransform.rect.height + 15);
 
         Vector2 randPosition;
+        int counter = 0;
+        int counterMax = 25;
 
         do
         {
             randPosition = GetRandPosition(islandSize);
+
+            float angle = Random.Range(0f, 360f);
+            float distance = counter * 2f;
+            Vector2 offset = new Vector2(Mathf.Cos(angle), Mathf.Sin(angle)) * distance;
+
+            randPosition += offset;
+
+            counter++;
+
+            if (counter > counterMax)
+            {
+                Debug.LogError("Max attempts reached for island: " + island.name);
+                return;
+            }
         }
-        while(IsOverlapping(randPosition, islandSize));
+        while (IsOverlapping(randPosition, islandSize, 15));
 
         rectTransform.anchoredPosition = randPosition;
         placedPositions.Add(randPosition);
@@ -74,27 +126,27 @@ public class CreateMap : MonoBehaviour
     {
         RectTransform rectTransform = island.GetComponent<RectTransform>();
 
-        if(rectTransform == null)
+        if (rectTransform == null)
         {
             Debug.LogError("No RectTransform found on island: " + island.name);
             return;
         }
 
-        Vector2 islandSize = new Vector2(rectTransform.rect.width, rectTransform.rect.height);
+        Vector2 islandSize = new Vector2(rectTransform.rect.width + 15, rectTransform.rect.height + 15);
         Vector2 randPosition;
 
         float setYPosition = rectTransform.anchoredPosition.y;
 
         do
         {
-            float xMin = boundingAreaCenter.x - boundingAreaSize.x / 2 + islandSize.x / 2;
+            float xMin = (boundingAreaCenter.x - boundingAreaSize.x / 2 + islandSize.x / 2) + 300;
             float xMax = boundingAreaCenter.x + boundingAreaSize.x / 2 - islandSize.x / 2;
 
             float randX = Random.Range(xMin, xMax);
 
             randPosition = new Vector2(randX, setYPosition);
-        } 
-        while (IsOverlapping(randPosition, islandSize));
+        }
+        while (IsOverlapping(randPosition, islandSize, 10));
 
         rectTransform.anchoredPosition = randPosition;
         placedPositions.Add(randPosition);
@@ -106,19 +158,19 @@ public class CreateMap : MonoBehaviour
     void AssignTIImg()
     {
         int randIndex = Random.Range(0, tiImages.Length);
-        treasureIslandImage.sprite = tiImages[randIndex]; 
+        treasureIslandImage.sprite = tiImages[randIndex];
     }
 
     void OnDrawGizmos()
     {
-        Gizmos.color = boxColour; 
+        Gizmos.color = boxColour;
 
-        foreach(GameObject island in centerIslands)
+        foreach (GameObject island in centerIslands)
         {
             DrawBoundingBox(island);
         }
 
-        foreach(GameObject island in edgeIslands)
+        foreach (GameObject island in edgeIslands)
         {
             DrawBoundingBox(island);
         }
@@ -128,7 +180,7 @@ public class CreateMap : MonoBehaviour
     {
         RectTransform rt = island.GetComponent<RectTransform>();
 
-        if(rt == null)
+        if (rt == null)
         {
             return;
         }
@@ -157,26 +209,48 @@ public class CreateMap : MonoBehaviour
         return new Vector2(randX, randY);
     }
 
-    bool IsOverlapping(Vector2 newPosition, Vector2 islandSize)
-    {
-        Rect newIslandRect = new Rect (
-            newPosition - islandSize / 2,
-            islandSize
-        );
-
-        foreach(Vector2 placedPosition in placedPositions)
+    /*
+        bool IsOverlapping(Vector2 newPosition, Vector2 islandSize)
         {
-            Rect placedIslandRect = new Rect (
-            (placedPosition - islandSize / 2,
-            islandSize
+            Rect newIslandRect = new Rect (
+                newPosition - islandSize / 2,
+                islandSize
             );
 
-            if(newIslandRect.Overlaps(placedIslandRect))
+            foreach(Vector2 placedPosition in placedPositions)
+            {
+                Rect placedIslandRect = new Rect (
+                placedPosition - islandSize / 2,
+                islandSize
+                );
+
+                if(newIslandRect.Overlaps(placedIslandRect))
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+    */
+
+    bool IsOverlapping(Vector2 newPosition, Vector2 islandSize, float minDistance)
+    {
+        Rect newIslandBounds = new Rect(newPosition - (islandSize / 2), islandSize);
+
+        foreach (Vector2 placedPos in placedPositions)
+        {
+            Rect placedIslandBounds = new Rect(placedPos - (islandSize / 2), islandSize);
+
+            if (newIslandBounds.xMax + minDistance > placedIslandBounds.xMin &&
+                newIslandBounds.xMin - minDistance < placedIslandBounds.xMax &&
+                newIslandBounds.yMax + minDistance > placedIslandBounds.yMin &&
+                newIslandBounds.yMin - minDistance < placedIslandBounds.yMax)
             {
                 return true;
             }
         }
-
         return false;
     }
+
 }
