99713c67ac63f38963e5c960cbe01b8532bfe18c
vincentyu22 <132400993+vincentyu22@users.noreply.github.com>
2024-10-18 08:45:17 +1300

    Squashed commit of the following:
    commit 44ef2e01d760ade9c7eebd8f038208b98bfe2b62
    Author: vincentyu22 <132400993+vincentyu22@users.noreply.github.com>
    Date:   Fri Oct 18 08:33:13 2024 +1300
    
        merge
    
    commit 2ce86c5bc85c74f7f84413657241a68e598be9d4
    Author: vincentyu22 <132400993+vincentyu22@users.noreply.github.com>
    Date:   Fri Oct 18 08:33:01 2024 +1300
    
        merge
    
    commit 8ecf635a9edcdfc1c9e28376f313d58ab6cdd5b4
    Merge: 84e856e 680544d
    Author: vincentyu22 <132400993+vincentyu22@users.noreply.github.com>
    Date:   Fri Oct 18 08:26:51 2024 +1300
    
        merge
    
    commit 84e856e050cca736b874d89c314a50b0b6b22b53
    Author: vincentyu22 <132400993+vincentyu22@users.noreply.github.com>
    Date:   Tue Oct 15 19:49:38 2024 +1300
    
        fix some bug and add the use and drop for the backpack
    
    commit ff70f6ad02448d86a1905d729138e23c4462d679
    Author: vincentyu22 <132400993+vincentyu22@users.noreply.github.com>
    Date:   Fri Oct 11 00:45:47 2024 +1300
    
        add  backpack Item Action Menu
    
    commit ee41e65e55175a8928523f498b3429eb5b5298a0
    Author: vincentyu22 <132400993+vincentyu22@users.noreply.github.com>
    Date:   Thu Oct 10 21:09:07 2024 +1300
    
        add
    
    commit 28601218ccc4d327efba95107d35fd2de585f60b
    Author: vincentyu22 <132400993+vincentyu22@users.noreply.github.com>
    Date:   Thu Oct 10 19:02:47 2024 +1300
    
        add edible and remove item
    
    commit bce738e52a09283b60066505c2d44280535cccce
    Author: vincentyu22 <132400993+vincentyu22@users.noreply.github.com>
    Date:   Thu Oct 3 06:33:06 2024 +1300
    
        add
    
    commit d0b4af9e511eaabc1ab9b9d22c7db252c0fd9c60
    Author: vincentyu22 <132400993+vincentyu22@users.noreply.github.com>
    Date:   Mon Sep 30 03:03:31 2024 +1300
    
        test
    
    commit b88bb68aa4873e552b65e1827d500b6bedd01465
    Author: vincentyu22 <132400993+vincentyu22@users.noreply.github.com>
    Date:   Mon Sep 30 03:00:25 2024 +1300
    
        Delete Combat.unity.meta
    
    commit d663d7f8701276efb42a2b95a35e3f04a0521836
    Author: vincentyu22 <132400993+vincentyu22@users.noreply.github.com>
    Date:   Fri Sep 20 01:26:25 2024 +1200
    
        add
    
    commit 2d720e0e169e1be07b9cc9e50988d9601e08b10b
    Author: vincentyu22 <132400993+vincentyu22@users.noreply.github.com>
    Date:   Fri Sep 20 01:23:06 2024 +1200
    
        add

--- My project (1)/Assets/Scripts/BackPack/Model/InventorySO.cs
+++ My project (1)/Assets/Scripts/BackPack/Model/InventorySO.cs
@@ -25,7 +25,7 @@ namespace Inventory.Model
                 inventoryItems.Add(InventoryItem.GetEmptyItem());
             }
         }
-        public int AddItem(ItemSO item, int quantity)
+        public int AddItem(ItemSO item, int quantity, List<ItemParameter> itemState = null)
         {
             if (item.IsStackable == false)
             {
@@ -35,7 +35,7 @@ namespace Inventory.Model
 
                     while (quantity > 0 && IsInventoryFull() == false) 
                     {
-                       quantity -=  AddItemToFirstFreeSlot(item, 1);
+                       quantity -=  AddItemToFirstFreeSlot(item, 1,itemState);
                       
 
                     }
@@ -49,12 +49,12 @@ namespace Inventory.Model
 
     }
 
-        private int AddItemToFirstFreeSlot(ItemSO item, int quantity)
+        private int AddItemToFirstFreeSlot(ItemSO item, int quantity, List<ItemParameter> itemState = null)
         {
             InventoryItem newItem = new InventoryItem
             {
                 item = item,
-                quantity = quantity
+                quantity = quantity,itemState = new List<ItemParameter> (itemState == null ? item.DefaultParametersList : itemState)
             };
 
             for (int i = 0; i < inventoryItems.Count; i++)
@@ -136,7 +136,23 @@ namespace Inventory.Model
             AddItem(item.item, item.quantity);
         }
 
+  public void RemoveItem(int itemIndex, int amount)
+        {
+            if (inventoryItems.Count > itemIndex)
+            {
+                if (inventoryItems[itemIndex].IsEmpty)
+                    return;
 
+                int reminder = inventoryItems[itemIndex].quantity - amount;
+                if (reminder <= 0)
+                    inventoryItems[itemIndex] = InventoryItem.GetEmptyItem();
+                else
+                    inventoryItems[itemIndex] = inventoryItems[itemIndex]
+                        .ChangeQuantity(reminder);
+
+                InformAboutChange();
+            }
+        }
 
         public void SwapItem(int itemIndex_1, int itemIndex_2)
         {
@@ -150,6 +166,8 @@ namespace Inventory.Model
         {
             OnInventoryUpdated?.Invoke(GetCurrentInventoryState());
         }
+
+      
     }
 
 
@@ -161,7 +179,7 @@ namespace Inventory.Model
     {
         public int quantity;
         public ItemSO item;
-        // public List<ItemParameter> itemState;
+         public List<ItemParameter> itemState;
         public bool IsEmpty => item == null;
 
         public InventoryItem ChangeQuantity(int newQuantity)
@@ -170,7 +188,7 @@ namespace Inventory.Model
             {
                 item = this.item,
                 quantity = newQuantity,
-                //  itemState = new List<ItemParameter>(this.itemState)
+                 itemState = new List<ItemParameter>(this.itemState)
             };
         }
 
@@ -179,7 +197,7 @@ namespace Inventory.Model
             {
                 item = null,
                 quantity = 0,
-                // itemState = new List<ItemParameter>()
+                itemState = new List<ItemParameter>()
             };
     }
 
